a2f08451c5daaddeef780639fa629ef084ae799b
AJ Markow <alexanderjmarkow@gmail.com>
2021-01-11 15:38:23 -0800

    Add import statement for next image component in intro component.  Add octocat png to public assets directory.  Size image added to intro component.

--- components/Intro.js
+++ components/Intro.js
@@ -1,8 +1,13 @@
 import {Container, Input,h1,h4} from 'semantic-ui-react';
+import Image from 'next/image';
 
 export default function Intro() {
   return(
     <Container text>
+      <Image 
+        src="/Octocat.png" 
+        width='80'
+        height='80' />
       <h1>Analyze Your Repos</h1>
       <h4>Quickly fetch a list of your public repositories missing README.md files</h4>
       <br></br>
